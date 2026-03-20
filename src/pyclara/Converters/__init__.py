3e0a5caa870797570315c4cf35c9c4307778c42d
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-03-20 19:28:22 +0000

    Tidy to correct locations

--- src/pyclara/Converters/__init__.py
+++ src/pyclara/Converters/__init__.py
@@ -1,3 +1,4 @@
 from ._yaml2impactx import *
+from ._yaml2xsuite import *
 from ._yaml2acccoords import *
 
---
+++ src/pyclara/Converters/_yaml2xsuite.py
@@ -0,0 +1,4 @@
+try :
+    import xsuite
+except ImportError:
+    print("xsuite not found, cannot convert yaml to xsuite line")
