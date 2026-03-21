9d8dcd1971b4538020623fba5c0fd608b867b149
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-03-21 20:42:37 +0000

    Conversion range

--- src/pyclara/Converters/_elegant2xsuite.py
+++ src/pyclara/Converters/_elegant2xsuite.py
@@ -69,30 +69,50 @@ def elegant2xsuite(elegant_file,
             print("element type ", ee['TYPE'], " not recognised, skipping")
 
     # need to be uppercase
-    line_componenet_names = [e.upper() for e in lte[line_name]['LINE']]
+    line_component_names = [e.upper() for e in lte[line_name]['LINE']]
+
+    if start_element is None or start_element == "":
+        start_element = line_component_names[0]
+    if end_element is None or end_element == "":
+        end_element = line_component_names[-1]
+
+    # select components
+    subline_component_names = []
+
+    adding = False
+    istart = -1
+
+    for i, e in enumerate(line_component_names) :
+        if e == start_element :
+            adding = True
+            istart = i
+
+        if adding :
+            subline_component_names.append(e)
+
+        if e == end_element :
+            adding = False
 
     # create line
     xt_line = env.new_line(name = line_name,
-                           components = line_componenet_names)
+                           components = subline_component_names)
 
     # add twiss to environment if provided
     if elegant_twi is not None :
-        p0 = elegant_twi.getColumnValueList('pCentral0')[0]
-        betax = elegant_twi.getColumnValueList('betax')[0]
-        alphax = elegant_twi.getColumnValueList('alphax')[0]
-        etax = elegant_twi.getColumnValueList('etax')[0]
-        etaxp = elegant_twi.getColumnValueList('etaxp')[0]
+        p0 = elegant_twi.getColumnValueList('pCentral0')[istart]
+        betax = elegant_twi.getColumnValueList('betax')[istart]
+        alphax = elegant_twi.getColumnValueList('alphax')[istart]
+        etax = elegant_twi.getColumnValueList('etax')[istart]
+        etaxp = elegant_twi.getColumnValueList('etaxp')[istart]
 
-        betay = elegant_twi.getColumnValueList('betay')[0]
-        alphay = elegant_twi.getColumnValueList('alphay')[0]
-        etay = elegant_twi.getColumnValueList('etay')[0]
-        etayp = elegant_twi.getColumnValueList('etayp')[0]
-
-        print(p0)
-        print(betax, alphax, etax, etaxp)
-        print(betay, alphay, etay, etayp)
+        betay = elegant_twi.getColumnValueList('betay')[istart]
+        alphay = elegant_twi.getColumnValueList('alphay')[istart]
+        etay = elegant_twi.getColumnValueList('etay')[istart]
+        etayp = elegant_twi.getColumnValueList('etayp')[istart]
 
 
+        print("Twiss init x : ",betax, alphax, etax, etaxp)
+        print("Twiss init y : ",betay, alphay, etay, etayp)
         xtrack_twiss0 = _xtrack.TwissInit(betx = betax,
                                           alfx = alphax,
                                           dx = etax,
@@ -105,10 +125,10 @@ def elegant2xsuite(elegant_file,
         xt_line.set_particle_ref(pdg_id_0=11,
                                  p0c = 0.51099895069*p0)
 
-        xtrack_twiss = xt_line.twiss(method="6d",
+        xtrack_twiss = xt_line.twiss(method="4d",
                                      init=xtrack_twiss0)
 
-
+        xtrack_track = xt_line.track
 
     return env, xtrack_twiss0, xtrack_twiss
 
