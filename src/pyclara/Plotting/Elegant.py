54485948d9553b11eb05211943778683d2ee8ab3
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-03-21 21:26:29 +0000

    Ranges for plots

--- src/pyclara/Plotting/Elegant.py
+++ src/pyclara/Plotting/Elegant.py
@@ -2,42 +2,60 @@ import sdds as _sdds
 import matplotlib.pyplot as _plt
 import numpy as _np
 
-def All(elegant_twi) :
+def All(elegant_twi, start_element = None, end_element = None) :
     if isinstance(elegant_twi, str) :
         elegant_twi = _sdds.load(elegant_twi)
 
     _plt.subplot(5,1,1)
-    Beta(elegant_twi, vertical_stack=True)
+    Beta(elegant_twi, start_element=start_element, end_element=end_element, vertical_stack=True)
 
     _plt.subplot(5,1,2)
-    Alpha(elegant_twi, vertical_stack=True)
+    Alpha(elegant_twi, start_element=start_element, end_element=end_element, vertical_stack=True)
 
     _plt.subplot(5,1,3)
-    Psi(elegant_twi, vertical_stack=True)
+    Psi(elegant_twi, start_element=start_element, end_element=end_element, vertical_stack=True)
 
     _plt.subplot(5,1,4)
-    Eta(elegant_twi, vertical_stack=True)
+    Eta(elegant_twi, start_element=start_element, end_element=end_element, vertical_stack=True)
 
     _plt.subplot(5,1,5)
-    Energy(elegant_twi)
+    Energy(elegant_twi, start_element=start_element, end_element=end_element, vertical_stack=True)
 
+
+def _getRangeIndices(names, start_element, end_element) :
+    if start_element is not None and start_element != "" :
+        i_start = _np.where(names == start_element)[0][0]
+    else :
+        i_start = 0
+
+    if end_element is not None and end_element != "" :
+        i_end = _np.where(names == end_element)[0][0]
+    else :
+        i_end = len(names)
+
+    return i_start, i_end
 
 def Machine(elegant_ele, vertical_stack = False) :
     pass
 
-def Beta(elegant_twi, vertical_stack = False) :
+def Beta(elegant_twi, start_element = None, end_element = None, vertical_stack = False) :
+
     if isinstance(elegant_twi, str) :
         elegant_twi = _sdds.load(elegant_twi)
 
     s = _np.array(elegant_twi.getColumnValueList('s'))
+    names = _np.array(elegant_twi.getColumnValueList('ElementName'))
     betax = _np.array(elegant_twi.getColumnValueList('betax'))
     betay = _np.array(elegant_twi.getColumnValueList('betay'))
 
+    # find indicies
+    i_start, i_end = _getRangeIndices(names, start_element, end_element)
+
     # remove any offset in s
-    s = s - s[0]
+    s = s - s[i_start]
 
-    _plt.plot(s,betax, label=r'Elegant $\beta_x$')
-    _plt.plot(s,betay, label=r'Elegant $\beta_y$')
+    _plt.plot(s[i_start:i_end],betax[i_start:i_end], label=r'Elegant $\beta_x$')
+    _plt.plot(s[i_start:i_end],betay[i_start:i_end], label=r'Elegant $\beta_y$')
 
     if vertical_stack :
         _plt.xlabel('')
@@ -47,16 +65,20 @@ def Beta(elegant_twi, vertical_stack = False) :
     _plt.ylabel(r'$\beta_{x,y}$ [m]')
     _plt.legend()
 
-def Alpha(elegant_twi, vertical_stack = False) :
+def Alpha(elegant_twi, start_element = None, end_element = None, vertical_stack = False) :
     if isinstance(elegant_twi, str) :
         elegant_twi = _sdds.load(elegant_twi)
 
-    s = elegant_twi.getColumnValueList('s')
-    alphax = elegant_twi.getColumnValueList('alphax')
-    alphay = elegant_twi.getColumnValueList('alphay')
+    s = _np.array(elegant_twi.getColumnValueList('s'))
+    names = _np.array(elegant_twi.getColumnValueList('ElementName'))
+    alphax = _np.array(elegant_twi.getColumnValueList('alphax'))
+    alphay = _np.array(elegant_twi.getColumnValueList('alphay'))
 
-    _plt.plot(s,alphax, label=r'Elegant $\alpha_x$')
-    _plt.plot(s,alphay, label=r'Elegant $\alpha_y$')
+    # find indicies
+    i_start, i_end = _getRangeIndices(names, start_element, end_element)
+
+    _plt.plot(s[i_start:i_end],alphax[i_start:i_end], label=r'Elegant $\alpha_x$')
+    _plt.plot(s[i_start:i_end],alphay[i_start:i_end], label=r'Elegant $\alpha_y$')
 
     if vertical_stack :
         _plt.xlabel('')
@@ -67,16 +89,20 @@ def Alpha(elegant_twi, vertical_stack = False) :
     _plt.ylabel(r'$\beta_{x,y}$ [m]')
     _plt.legend()
 
-def Psi(elegant_twi, vertical_stack = False) :
+def Psi(elegant_twi, start_element = None, end_element = None, vertical_stack = False) :
     if isinstance(elegant_twi, str) :
         elegant_twi = _sdds.load(elegant_twi)
 
-    s = elegant_twi.getColumnValueList('s')
+    s = _np.array(elegant_twi.getColumnValueList('s'))
+    names = _np.array(elegant_twi.getColumnValueList('ElementName'))
     psix = _np.array(elegant_twi.getColumnValueList('psix'))/_np.pi
     psiy = _np.array(elegant_twi.getColumnValueList('psiy'))/_np.pi
 
-    _plt.plot(s,psix, label=r'Elegant $\psi_x [\pi]$')
-    _plt.plot(s,psiy, label=r'Elegant $\psi_y [\pi]$')
+    # find indicies
+    i_start, i_end = _getRangeIndices(names, start_element, end_element)
+
+    _plt.plot(s[i_start:i_end],psix[i_start:i_end], label=r'Elegant $\psi_x [\pi]$')
+    _plt.plot(s[i_start:i_end],psiy[i_start:i_end], label=r'Elegant $\psi_y [\pi]$')
 
     if vertical_stack :
         _plt.xlabel('')
@@ -86,16 +112,20 @@ def Psi(elegant_twi, vertical_stack = False) :
     _plt.ylabel(r'$\psi_{x,y} [\pi]$ ')
     _plt.legend()
 
-def Eta(elegant_twi, vertical_stack = False) :
+def Eta(elegant_twi, start_element = None, end_element = None, vertical_stack = False) :
     if isinstance(elegant_twi, str) :
         elegant_twi = _sdds.load(elegant_twi)
 
-    s = elegant_twi.getColumnValueList('s')
-    etax = elegant_twi.getColumnValueList('etax')
-    etay = elegant_twi.getColumnValueList('etay')
+    s = _np.array(elegant_twi.getColumnValueList('s'))
+    names = _np.array(elegant_twi.getColumnValueList('ElementName'))
+    etax = _np.array(elegant_twi.getColumnValueList('etax'))
+    etay = _np.array(elegant_twi.getColumnValueList('etay'))
 
-    _plt.plot(s,etax, label=r'Elegant $\eta_x$')
-    _plt.plot(s,etay, label=r'Elegant $\eta_y$')
+    # find indicies
+    i_start, i_end = _getRangeIndices(names, start_element, end_element)
+
+    _plt.plot(s[i_start:i_end],etax[i_start:i_end], label=r'Elegant $\eta_x$')
+    _plt.plot(s[i_start:i_end],etay[i_start:i_end], label=r'Elegant $\eta_y$')
 
     if vertical_stack :
         _plt.xlabel('')
@@ -106,19 +136,24 @@ def Eta(elegant_twi, vertical_stack = False) :
     _plt.ylabel(r'$\eta_{x,y}$ [m]')
     _plt.legend()
 
-def Energy(elegant_twi, vertical_stack = False) :
+def Energy(elegant_twi, start_element = None, end_element = None, vertical_stack = False) :
     if isinstance(elegant_twi, str) :
         elegant_twi = _sdds.load(elegant_twi)
 
-    s = elegant_twi.getColumnValueList('s')
-    pCentral0 = elegant_twi.getColumnValueList('pCentral0')
+    s = _np.array(elegant_twi.getColumnValueList('s'))
+    names = _np.array(elegant_twi.getColumnValueList('ElementName'))
+    pCentral0 = _np.array(elegant_twi.getColumnValueList('pCentral0'))
 
-    _plt.plot(s,pCentral0, label=r'$p_0$')
+    # find indicies
+    i_start, i_end = _getRangeIndices(names, start_element, end_element)
+
+    _plt.plot(s[i_start:i_end],pCentral0[i_start:i_end], label=r'$p_0$')
 
     if vertical_stack :
         _plt.xlabel('')
         _plt.gca().xaxis.set_visible(False)
     else :
         _plt.xlabel("s [m]")
-        _plt.ylabel(r"Elegant $\beta\gamma$")
+
+    _plt.ylabel(r"Elegant $\beta\gamma$")
     _plt.legend()
--- src/pyclara/Plotting/XSuite.py
+++ src/pyclara/Plotting/XSuite.py
@@ -7,17 +7,22 @@ def All(xsuite_twiss) :
     Beta(xsuite_twiss, vertical_stack=True)
 
     _plt.subplot(5,1,2)
+    Alpha(xsuite_twiss, vertical_stack=True)
+
+    _plt.subplot(5,1,3)
+    Psi(xsuite_twiss, vertical_stack=True)
+
+    _plt.subplot(5,1,4)
     Eta(xsuite_twiss, vertical_stack=False)
 
 
-def Beta(xsuite_twiss, linestyle = "-", marker=None, s0offset = 0, vertical_stack = False) :
+def Beta(xsuite_twiss, linestyle = "-", marker=None, sOffset = 0, vertical_stack = False) :
     s = _np.array(xsuite_twiss['s'])
     betax = _np.array(xsuite_twiss['betx'])
     betay = _np.array(xsuite_twiss['bety'])
 
     # remove any offset in s
-    s = s - s[0]
-    s = s + s0offset
+    s = s + sOffset
 
     _plt.plot(s,betax, label=r'XSuite $\beta_x$', linestyle = linestyle, marker = marker)
     _plt.plot(s,betay, label=r'XSuite $\beta_y$', linestyle = linestyle, marker = marker)
@@ -31,12 +36,53 @@ def Beta(xsuite_twiss, linestyle = "-", marker=None, s0offset = 0, vertical_stac
     _plt.ylabel(r'$\beta_{x,y}$ [m]')
     _plt.legend()
 
+def Alpha(xsuite_twiss, linestyle="-", marker=None, s0offset=0, vertical_stack=False):
+    s = _np.array(xsuite_twiss['s'])
+    alphax = _np.array(xsuite_twiss['alfx'])
+    alphay = _np.array(xsuite_twiss['alfy'])
+
+    # remove any offset in s
+    s = s + sOffset
+
+    _plt.plot(s, alphax, label=r'XSuite $\alpha_x$', linestyle=linestyle, marker=marker)
+    _plt.plot(s, alphay, label=r'XSuite $\alpha_y$', linestyle=linestyle, marker=marker)
+
+    if vertical_stack:
+        _plt.xlabel('')
+        _plt.gca().xaxis.set_visible(False)
+    else:
+        _plt.xlabel("s [m]")
+
+    _plt.ylabel(r'$\alpha_{x,y}$ [m]')
+    _plt.legend()
+
+def Psi(xsuite_twiss, linestyle="-", marker=None, s0offset=0, vertical_stack=False):
+    s = _np.array(xsuite_twiss['s'])
+    psix = _np.array(xsuite_twiss['mux'])/_np.pi
+    psiy = _np.array(xsuite_twiss['muy'])/_np.pi
+
+    # remove any offset in s
+    s = s + sOffset
+
+    _plt.plot(s, psix, label=r'XSuite $\psi_x$', linestyle=linestyle, marker=marker)
+    _plt.plot(s, psiy, label=r'XSuite $\psi_y$', linestyle=linestyle, marker=marker)
+
+    if vertical_stack:
+        _plt.xlabel('')
+        _plt.gca().xaxis.set_visible(False)
+    else:
+        _plt.xlabel("s [m]")
+
+    _plt.ylabel(r'$\psi_{x,y}$ [$\pi$] ')
+    _plt.legend()
+
 def Eta(xsuite_twiss, linestyle = "-", marker=None, s0offset = 0, vertical_stack = False) :
     s = _np.array(xsuite_twiss['s'])
     etax = _np.array(xsuite_twiss['dx'])
     etay = _np.array(xsuite_twiss['dy'])
 
-    s = s + s0offset
+    # remove any offset in s
+    s = s + sOffset
 
     _plt.plot(s,etax, label=r'XSuite $\eta_x$', linestyle = linestyle, marker = marker)
     _plt.plot(s,etay, label=r'XSuite $\eta_y$', linestyle = linestyle, marker = marker)
@@ -49,3 +95,4 @@ def Eta(xsuite_twiss, linestyle = "-", marker=None, s0offset = 0, vertical_stack
 
     _plt.ylabel(r'$\eta_{x,y}$ [m]')
     _plt.legend()
+
